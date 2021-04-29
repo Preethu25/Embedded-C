ff43466cb55b0226de3cf482760c1d9c1d315cb4
Preethu25 <80700297+Preethu25@users.noreply.github.com>
2021-04-29 23:15:59 +0530

    Delete act2_tempsensor.h

--- inc/act2_tempsensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef ACT2_TEMPSENSOR_H_INCLUDED
-#define ACT2_TEMPSENSOR_H_INCLUDED
-
-#include <avr/io.h>
-
-void initiADC();
-uint16_t ReadADC(uint8_t ch);
-
-#endif // ACT2_TEMPSENSOR_H_INCLUDED

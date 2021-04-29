39d25fb0754066d26243b067c119e7cc8384e29c
Preethu25 <80700297+Preethu25@users.noreply.github.com>
2021-04-29 19:09:32 +0530

    Update act1buttonsensor.h

--- inc/act1buttonsensor.h
+++ inc/act1buttonsensor.h
@@ -3,8 +3,8 @@
 
 #include <avr/io.h>
 
-#define Heater_ON !(PIND&(1<<PD0)) // Switch 1 is ON
-#define ButtonSensor_ON !(PIND&(1<<PD1)) // Switch 2 is ON
+#define ButtonSensor_ON !(PIND&(1<<PD0)) // Switch 1 is ON
+#define Heater_ON !(PIND&(1<<PD1)) // Switch 2 is ON
 
 void initiLED(void);
 

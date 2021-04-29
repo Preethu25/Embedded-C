8577e870e42aa4fc6a0b597022c5493a2f3e0943
Preethu25 <80700297+Preethu25@users.noreply.github.com>
2021-04-29 23:00:35 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <avr/io.h>
 #include "act1buttonsensor.h"
+#include "act2_tempsensor.h"
+
+uint16_t temp1;
 int main(void)
 {
     initiLED();
+    initiADC();
     while(1)
     {
           if(ButtonSensor_ON && Heater_ON)
           {
                   PORTB|=(1<<PB0);
+                  temp1=ReadADC(0);
           }
           else
         {

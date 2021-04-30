d22124199eebf03d772f7e527e7f96b52d7b79e5
Preethu25 <80700297+Preethu25@users.noreply.github.com>
2021-04-30 08:09:51 +0530

    Update act1buttonsensor.c

--- src/act1buttonsensor.c
+++ src/act1buttonsensor.c
@@ -1,10 +1,21 @@
+/**
+ * @file act1buttonsensor.c
+ * @author 259870 (you@domain.com)
+ * @brief Turning the LED ON and OFF
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include <avr/io.h>
 #include "act1buttonsensor.h"
 
-void initiLED(void)
+void initiLED(void)  // Initializing the input and output ports
 {
-	DDRB|=(1<<PB0);
-    DDRD&=~(1<<PD0);
-    PORTD|=(1<<PD0);
-    PORTD|=(1<<PD1);
+    DDRB|=(1<<PB0);  //Set PB0=1 for LED
+    DDRD&=~(1<<PD0); // Clear bit 
+    PORTD|=(1<<PD0); // Set bit of D0
+    PORTD|=(1<<PD1); // Set bit of D1
 }

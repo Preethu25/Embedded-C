cab3aad572269e6c4f1428dd6961f7351e0d23de
Preethu25 <80700297+Preethu25@users.noreply.github.com>
2021-04-30 12:27:17 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,30 @@
+/*
+ */
+
 #include <avr/io.h>
 #include "act1buttonsensor.h"
 #include "act2tempsensor.h"
+#include"act3PWMoutput.h"
 
 uint16_t temp1;
+char temp2;
 int main(void)
 {
     initiLED();
     initiADC();
+    initiPWM();
     while(1)
     {
           if(ButtonSensor_ON && Heater_ON)
           {
                   PORTB|=(1<<PB0);
                   temp1=ReadADC(0);
+                  temp2 = PWMoutput(temp1);
           }
           else
         {
               PORTB&=~(1<<PB0);
+              OCR1A=0;
         }
     }
 

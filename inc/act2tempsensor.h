41f204bc73ba17c3ad78b85ddc8864679c07bd31
Preethu25 <80700297+Preethu25@users.noreply.github.com>
2021-04-30 23:40:01 +0530

    Update act2tempsensor.h

--- inc/act2tempsensor.h
+++ inc/act2tempsensor.h
@@ -1,3 +1,14 @@
+/**
+ * @file act2tempsensor.h
+ * @author Preethu R()
+ * @brief ADC Channels
+ * @version 0.1
+ * @date 2021-04-30
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 #ifndef ACT2TEMPSENSOR_H_INCLUDED
 #define ACT2TEMPSENSOR_H_INCLUDED
 
